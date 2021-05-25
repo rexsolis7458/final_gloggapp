53e87a032f9d76dd8b1a629aba476684d21af9ec
rex <rexbryan.solis.20@usjr.edu.ph>
2021-05-25 15:17:09 +0800

    change

--- lib/screens/add.dart
+++ lib/screens/add.dart
@@ -266,7 +266,7 @@ class AddRecipe extends StatelessWidget {
                       ),
                     ),
                   ),
-                )
+                ),
               ],
             )));
   }
